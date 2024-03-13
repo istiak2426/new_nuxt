2158bcafdf0448ef76a2cc0ae81da37b78d30fe8
istiak2426 <istiak2426@gmail.com>
2024-03-13 21:59:11 +0600

    add css2

--- components/Boxone.vue
+++ components/Boxone.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <UContainer class="mt-10 w-full h-96">
+    <UContainer class="mt-10 w-full h-[70vh]">
       <UCard class="h-full">
         <div
           class="mt-[-45px] bg-[#007c9d] p-5 m-6 rounded-lg shadow-xl text-xl text-white font-bold"
@@ -52,15 +52,17 @@
           </div>
         </div>
 
-        <div class="text-xl text-white flex justify-center ">
-          <div class="flex items-center w-[900px] h-12 p-3 border-solid border-2 border-slate-600">
-            <span class="ml-2 text-[#FC9A00]"
-              >* Ensure that your site does not have a firewall or cross-origin
-              restrictions blocking the crawler.</span
-            >
+        <div
+          class="text-xl flex flex-col justify-center items-center h-[20vh] p-3 border-solid border-2 border-slate-600"
+        >
+          <div class="ml-2 text-black">
+            To obtain PageSpeed and Web Vitals data, please use an API key.
+          </div>
+
+          <div class="ml-2 mt-2 text-black">
+            To obtain PageSpeed and Web Vitals data, please use an API key.
           </div>
         </div>
-
       </UCard>
     </UContainer>
   </div>
